271f3a3a7dadc348755c602e0890967e9e6d0a0d
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-12-12 01:34:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ function fareTripler(x){
 }
 
 function selectDifferentDrivers(drivers, returnDrivers){
-  return returnFirstTwoDrivers; if (returnDrivers === returnFirstTwoDrivers);
-   
+  return drivers.slice(0, 2); if (returnDrivers === returnFirstTwoDrivers);
+
 }
 
 // describe('selectDifferentDrivers(arrayOfDrivers, function)', function () {
