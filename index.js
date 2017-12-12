da85cc352dead946254d11b55722ca3c515bacee
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-12-12 01:27:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Code your solution in this file!
 const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
 
-const returnFirstTwoDrivers = function (drivers){
+const firstTwo = function returnFirstTwoDrivers(drivers){
   return drivers.slice(0, 2);
 }
 
-const returnLastTwoDrivers = function (drivers){
+const lastTwo = function returnLastTwoDrivers(drivers){
   return drivers.slice(2, 4);
 }
 
