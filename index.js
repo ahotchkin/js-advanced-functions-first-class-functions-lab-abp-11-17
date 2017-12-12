3f14f576383c0764a16107a2aaa54f3c8f0798fd
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-12-12 01:23:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function (drivers){
+  return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function (drivers){
+  return drivers.slice(2, 4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(x, fareMultiplier){
+  return function fareMultiplier() {
+    return Math.pow(x, 2)
+  }
+}
+
+function fareDoubler(x){
+  return (x*2)
+}
+
+function fareTripler(x){
+  return (x*3)
+}
+
+function selectDifferentDrivers(drivers, select){
+  //return drivers.slice(0, 2) if (select === returnFirstTwoDrivers)
+}
+
+// describe('selectDifferentDrivers(arrayOfDrivers, function)', function () {
+//   it('returns the first two drivers when passed returnFirstTwoDrivers() as the second argument', function () {
+//     expect(selectDifferentDrivers(drivers, returnFirstTwoDrivers)).to.eql(['Sally', 'Bob']);
+//   });
+//
+//   it('returns the last two drivers when passed returnLastTwoDrivers() as the second argument', function () {
+//     expect(selectDifferentDrivers(drivers, returnLastTwoDrivers)).to.eql(['Freddy', 'Claudia']);
+//   });
+// });
